5fe52aaf48d43b0e2a9706fa240f1a43ff0e3507
arthurgao <arthurerconggao@gmail.com>
2023-03-11 01:57:04 -0500

    [NEW] startup shell scripts

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source ./venv/bin/activate
+python3 restify/manage.py makemigrations
+python3 restify/manage.py migrate
+python3 restify/manage.py runserver
---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 -m venv ./venv/
+python3 -m pip install --upgrade pip
+python3 -m pip install django
+python3 -m pip install djangorestframework
+python3 -m pip install --upgrade Pillow
+python3 -m pip install djangorestframework-simplejwt
