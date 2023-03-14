92e76b7dde44de372d952e522a110282ecabe595
dinofio <harry.duong@mail.utoronto.ca>
2023-03-14 14:21:20 -0400

    created a django app for comments

--- restify/comments/models.py
+++ restify/comments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-from .property import PropertyModel
+from ..properties.models.property import PropertyModel
 
 
 # TODO Change - merge comment models
